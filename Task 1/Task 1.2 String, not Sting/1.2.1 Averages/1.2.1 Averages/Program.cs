ee869233aec4224e86732374f32a5556c7263f16
Asssssiya <96336008+Asssssiya@users.noreply.github.com>
2022-01-17 21:53:14 +0300

    Averages
    Task 1.2.1

---
+++ Task 1/Task 1.2 String, not Sting/1.2.1 Averages/1.2.1 Averages/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _1._2._1_Averages
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите текст:");
+            string s = Console.ReadLine();
+            
+            int amount = GetWordsCount(s);
+            int amountofletter = GetLettersCount(s);
+            int average = GetAverageAmount(amountofletter, amount); // значение переменной округляется в меньшую сторону
+            Console.WriteLine($"Среднее слово строки состоит из: {average} букв!");
+        }
+
+        private static int GetWordsCount(string s)
+        {
+            string[] words = s.Split(' ');
+
+            int wordscount = 0;
+
+            foreach ( string word in words )
+            {
+                int letterscount = GetLettersCount(word);
+                                               
+                if (letterscount > 0)
+                {
+                    wordscount += 1;
+                }
+            }
+                        
+            return wordscount;
+        }
+
+        private static int GetLettersCount(string s)
+        {
+
+            int count = 0;
+                        
+            foreach (  char letter in s)
+            {
+                                
+                if (char.IsLetter(letter))
+                {
+                    count += 1;
+                }
+               
+            }
+
+            return count;
+        }
+
+        private static int GetAverageAmount(int letterscount, int wordscount)
+        {
+            int average = letterscount / wordscount;
+
+            return average;
+        }
+
+    }
+}
