0790063d079086b67b3b2fd83c14b4d174dabcc9
Asssssiya <96336008+Asssssiya@users.noreply.github.com>
2022-01-17 22:46:10 +0300

    1.2.2 Doubler

---
+++ Task 1/Task 1.2 String, not Sting/1.2.2 Doubler/1.2.2 Doubler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _1._2._2_Doubler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку, которую будем изменять:");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("Введите вторую строку:");
+            string second = Console.ReadLine();
+
+            string third = DoubleCharsString(first, second);
+                
+                Console.WriteLine($"Модифицированнная строка: {third}");
+        }
+
+        public static string DoubleCharsString(string firststring, string secondstring)
+        {
+
+            System.Text.StringBuilder doublestring = new System.Text.StringBuilder();
+
+            foreach (char charfromfirststring in firststring)
+            {
+
+                if (secondstring.Contains(charfromfirststring))
+                {
+                    doublestring.Append(charfromfirststring);
+                    doublestring.Append(charfromfirststring);
+                }
+
+                 else   
+                {
+                    doublestring.Append(charfromfirststring);
+                }
+                               
+                
+            }
+
+            return doublestring.ToString();
+        }
+    }
+}
