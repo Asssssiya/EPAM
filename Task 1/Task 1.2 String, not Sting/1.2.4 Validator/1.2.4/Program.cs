586c4a7a21da33a01207d09d00f47bf1ee6ec1eb
Asssssiya <96336008+Asssssiya@users.noreply.github.com>
2022-01-18 22:26:17 +0300

    1.2.4

---
+++ Task 1/Task 1.2 String, not Sting/1.2.4 Validator/1.2.4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System; 
+using System.Text;
+
+namespace _1._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string lowletterstring = Console.ReadLine();
+            string modifiedtext = Modify(lowletterstring);
+            Console.WriteLine($"Модифицированный текст:\n{modifiedtext}"); ;
+        }
+
+      
+        private static string Modify(string str)
+        {
+            StringBuilder builder = new StringBuilder();
+            bool nextuppercase = true;
+
+            foreach (char letter in str)
+            {
+                if (char.IsLetter(letter) == true && nextuppercase)
+                {
+                    char big = Char.ToUpper(letter);
+                    builder.Append(big);
+                    nextuppercase = false;
+                }
+                else
+                {
+                    builder.Append(letter);
+                }
+
+                if(letter == '.'|| letter == '!'|| letter == '?')
+                {
+                    nextuppercase = true;
+
+                }
+            }
+
+            return builder.ToString();
+        }
+
+
+
+
+
+    } 
+}
