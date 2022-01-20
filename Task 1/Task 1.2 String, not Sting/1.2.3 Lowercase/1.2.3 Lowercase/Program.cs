a828cde58e675c22586df3a5742c9a4b67181618
Asssssiya <96336008+Asssssiya@users.noreply.github.com>
2022-01-20 19:50:19 +0300

    1.2.3

---
+++ Task 1/Task 1.2 String, not Sting/1.2.3 Lowercase/1.2.3 Lowercase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _1._2._3_Lowercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string inputString = Console.ReadLine();
+            int wordsWithLowLetter = GetWordsWithLowLetterCount(inputString);
+            Console.WriteLine("Количество словс маленькой буквы: " + wordsWithLowLetter);
+        }
+
+        private static int GetWordsWithLowLetterCount(string insertedText)
+        {
+            int wordsWithLowLetterCount = 0;
+
+            string[] words = insertedText.Split(' ');
+
+
+            foreach (string word in words)
+            {
+                if (word.Length == 0)
+                {
+                    continue;
+                }
+
+                char firstLetter = word[0];
+
+                
+                if (char.IsLower(firstLetter))
+                {
+                    wordsWithLowLetterCount += 1;
+                }
+
+            }
+
+
+            return wordsWithLowLetterCount;
+
+        }
+
+    }
+}
