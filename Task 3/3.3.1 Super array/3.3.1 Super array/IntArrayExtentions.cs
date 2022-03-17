df138f4e9fdb41e288f7f3d6b365e832b55c7437
Asssssiya <96336008+Asssssiya@users.noreply.github.com>
2022-03-17 23:32:46 +0300

    Task 3
    3.3.1 Super array

---
+++ Task 3/3.3.1 Super array/3.3.1 Super array/IntArrayExtentions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._3._1_Super_array
+{
+    public static class IntArrayExtentions
+    {
+        public static int Sum(this int[] numbers)
+        {
+            return numbers.Sum(number => number);
+        }
+
+        public static double Average(this int[] numbers)
+        {           
+            return (double)numbers.Sum(number => number) / numbers.Length;            
+        }
+
+        public static int MostFrequentElement(this int[] numbers)
+        {
+            return numbers
+                .GroupBy(number => number)
+                .OrderByDescending(group => group.Count())
+                .First()
+                .Key;
+        }
+
+        public static int[] Apply(this int[] numbers, Func<int, int> function)
+        {
+            return numbers.Select(function).ToArray();
+        }
+    }
+
+   
+}
---
+++ Task 3/3.3.1 Super array/3.3.1 Super array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3._3._1_Super_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 4, 5, 2, 6, 5, 4, 3 };
+                        
+            int[] newArray = array.Apply(MultiplyBy2);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+           
+        }
+
+        static int MultiplyBy2(int number)
+        {
+            return number * 2;
+        }
+    }
+}
